0962d8ab5b4156aa2f6608368ee8a08a6458fec3
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2021-12-14 19:10:42 -0800

    wrote first test to change price of pizza based on size of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,20 @@ function Order() {
   this.total = 0;
 }
 
-function Pizza(size, crust, sauce, price) {
+function Pizza(size, crust, sauce,) {
   this.size = size;
   this.crust = crust;
   this.sauce = sauce;
-  this.price = price
   this.toppings = [];
+}
+
+Pizza.prototype.calculateCost = function () {
+  if (this.size === 10) {
+    this.price = 7;
+  } else if (this.size === 12) {
+    this.price = 10;
+  } else {
+    this.price = 14;
+  }
+  return this.price
 }
