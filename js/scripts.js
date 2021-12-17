53b0c58c89580d92bcc91826d00d54420471da98
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2021-12-17 13:48:51 -0800

    added UI logic to take in inputs and add the created pizza inside Order

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,32 @@ Order.prototype.addPizza = function (pizza) {
 Order.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
-};
+}
 
 Order.prototype.findPizza = function(id) {
   if (this.pizzas[id] != undefined) {
     return this.pizzas[id];
   }
   return false;
+}
+
+Order.prototype.deletePizza = function (id) {
+  for (let i = 0; i < this.pizzas.length; i++) {
+    if (this.pizzas[i]) {
+      if (this.pizzas[i].id == id) {
+        delete this.pizzas[i];
+        return true;
+      }
+    }
+  }
+  return false;
 };
-function Pizza(person, size, crust, sauce,) {
+function Pizza(person, size, crust, sauce, toppings) {
   this.person = person;
   this.size = size;
   this.crust = false;
   this.sauce = sauce;
-  this.toppings = [];
+  this.toppings = toppings;
 }
 
 Pizza.prototype.calculateCost = function () {
@@ -54,3 +66,23 @@ Pizza.prototype.calculateCost = function () {
   this.price = crustPrice + sizePrice + toppingsPrice;
   return this.price
 }
+
+let order = new Order();
+
+
+$(document).ready(function () {
+  $("form#createPizza").submit(function (event) {
+    event.preventDefault();
+    tops = []
+    const inputtedName = $("input#name").val();
+    const inputtedSize = $("select#size").val();
+    const inputtedSauce = $("select#sauce").val();
+    const inputtedToppings = $("input:checkbox[name=top]:checked").each(function(){
+      const toppings = $(this).val()
+      tops.push(toppings)
+      console.log(inputtedName);
+    });
+    let newPizza = new Pizza(inputtedName, inputtedSize, false, inputtedSauce, tops);
+    order.addPizza(newPizza);
+  })
+})
